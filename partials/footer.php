f9cf27ebcd18e295a8761545004a8becdb44d29d
Ye Htut Win <yehtutwin.tech@gmail.com>
2024-12-19 19:43:10 +0630

    partials - header, sidebar, footer

---
+++ partials/footer.php
@@ -0,0 +1,11 @@
+
+      </main>
+    </div>
+  </div>
+  <script src="../assets/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+  <script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js" integrity="sha384-eI7PSr3L1XLISH8JdDII5YN/njoSsxfbrkCTnJrzXt+ENP5MOVBxD+l6sEG4zoLp" crossorigin="anonymous"></script>
+  <script src="../assets/dashboard.js"></script>
+</body>
+
+</html>
