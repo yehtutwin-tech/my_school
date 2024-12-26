779b423653c13cb5b6cb1491ddcd7fa1de46efa7
Ye Htut Win <yehtutwin.tech@gmail.com>
2024-12-26 19:58:27 +0630

    registration process - done

--- auth/process.php
+++ auth/process.php
@@ -1,17 +1,36 @@
 <?php
+include_once '../config/db.php';
 
-// $name = $_POST['name'];
-// $email = $_POST['email'];
-// $password = $_POST['password'];
-// $confirm_password = $_POST['confirm_password'];
+function register($conn) {
+  if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    header('Location: register.php');
+    exit();
+  }
 
-// echo $name;
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  $confirm_password = $_POST['confirm_password'];
+  
+  if ($password !== $confirm_password) {
+    header('Location: register.php');
+    exit();
+  }
+
+  $password = password_hash($password, PASSWORD_BCRYPT);
+  
+  $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+
+  $conn->query($sql);
+
+  header('Location: login.php');
+}
 
 $action = $_GET['action'];
 
 switch ($action) {
   case 'register':
-    echo 'register';
+    register($conn);
     break;
   case 'login':
     echo 'login';
